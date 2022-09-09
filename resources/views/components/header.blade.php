38793898107aefffa179ba7bf6f9303e0091c359
DailyLollipops <clarencemadrigal84@gmail.com>
2022-09-09 23:06:27 +0800

    Some header changes

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,14 +1,9 @@
 @props([])
 
-<header class="u-clearfix u-grey-5 u-header u-sticky u-sticky-6c84 u-header" id="sec-3e0f" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="">
-  <style class="u-sticky-style" data-style-id="6c84">.u-sticky-fixed.u-sticky-6c84:before, .u-body.u-sticky-fixed .u-sticky-6c84:before {
-    borders: top right bottom left !important
-  }
-  </style>
+<header class="u-clearfix u-grey-5 u-header u-sticky u-sticky-6c84 u-valign-middle u-header" id="sec-3e0f" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="">
   <a href="/medbot/public/" class="u-image u-logo u-image-1" data-image-width="229" data-image-height="220">
-    <img src="{{ asset ('images/logo.png')}}" class="u-logo-image u-logo-image-1">
+    <img src="{{ asset('images/logo.png') }}" class="u-logo-image u-logo-image-1">
   </a>
-  {{-- <p class="u-custom-font u-font-lobster u-text u-text-default u-text-1">Med-bot</p> --}}
   <nav class="u-align-left u-menu u-menu-one-level u-offcanvas u-menu-1">
     <div class="menu-collapse">
       <a class="u-button-style u-nav-link" href="#">
@@ -39,7 +34,7 @@
     </div>
     <div class="u-custom-menu u-nav-container-collapse">
       <div class="u-align-center u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
-        <div class="u-sidenav-overflow">
+        <div class="u-inner-container-layout u-sidenav-overflow">
           <div class="u-menu-close"></div>
           <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2">
             <li class="u-nav-item">
@@ -49,7 +44,7 @@
               <a class="u-button-style u-nav-link" href="/medbot/public/about">About</a>
             </li>
             <li class="u-nav-item">
-              <a class="u-button-style u-nav-link" href="/medbot/public/login">Login</a>
+              <a class="u-button-style u-nav-link" href="/medbot/public/login/user">Login</a>
             </li>
           </ul>
         </div>
@@ -57,4 +52,7 @@
       <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
     </div>
   </nav>
+  <h3 class="u-custom-font u-font-lobster u-headline u-text u-text-1">
+    <a href="/">Med-bot</a>
+  </h3>
 </header>
--- resources/views/components/headerAuth.blade.php
+++ resources/views/components/headerAuth.blade.php
@@ -1,14 +1,9 @@
 @props([])
 
-<header class="u-clearfix u-grey-5 u-header u-sticky u-sticky-6c84 u-header" id="sec-3e0f" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="">
-  <style class="u-sticky-style" data-style-id="6c84">.u-sticky-fixed.u-sticky-6c84:before, .u-body.u-sticky-fixed .u-sticky-6c84:before {
-    borders: top right bottom left !important
-  }
-  </style>
+<header class="u-clearfix u-grey-5 u-header u-sticky u-sticky-6c84 u-valign-middle u-header" id="sec-3e0f" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="">
   <a href="/medbot/public/" class="u-image u-logo u-image-1" data-image-width="229" data-image-height="220">
-    <img src="{{ asset ('images/logo.png')}}" class="u-logo-image u-logo-image-1">
+    <img src="{{ asset('images/logo.png') }}" class="u-logo-image u-logo-image-1">
   </a>
-  {{-- <p class="u-custom-font u-font-lobster u-text u-text-default u-text-1">Med-bot</p> --}}
   <nav class="u-align-left u-menu u-menu-one-level u-offcanvas u-menu-1">
     <div class="menu-collapse">
       <a class="u-button-style u-nav-link" href="#">
@@ -36,16 +31,16 @@
           <a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="/medbot/public/list" style="padding: 10px 20px;">Readings</a>
         </li>
         <li class="u-nav-item">
-          <a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="/medbot/public/login/user" style="padding: 10px 20px;">Manage</a>
+          <a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="/medbot/public/manage" style="padding: 10px 20px;">Manage</a>
         </li>
         <li class="u-nav-item">
-          <a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="/medbot/public/logout" style="padding: 10px 20px;">Logout</a>
+          <a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="/medbot/public/login/user" style="padding: 10px 20px;">Login</a>
         </li>
       </ul>
     </div>
     <div class="u-custom-menu u-nav-container-collapse">
       <div class="u-align-center u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
-        <div class="u-sidenav-overflow">
+        <div class="u-inner-container-layout u-sidenav-overflow">
           <div class="u-menu-close"></div>
           <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2">
             <li class="u-nav-item">
@@ -58,10 +53,10 @@
               <a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="/medbot/public/list" style="padding: 10px 20px;">Readings</a>
             </li>
             <li class="u-nav-item">
-              <a class="u-button-style u-nav-link" href="/medbot/public/login">Manage</a>
+              <a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="/medbot/public/manage" style="padding: 10px 20px;">Manage</a>
             </li>
             <li class="u-nav-item">
-              <a class="u-button-style u-nav-link" href="/medbot/public/logout">Logout</a>
+              <a class="u-button-style u-nav-link" href="/medbot/public/login/user">Login</a>
             </li>
           </ul>
         </div>
@@ -69,4 +64,7 @@
       <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
     </div>
   </nav>
+  <h3 class="u-custom-font u-font-lobster u-headline u-text u-text-1">
+    <a href="/">Med-bot</a>
+  </h3>
 </header>
