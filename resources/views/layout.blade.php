254f69ad5cb22b1cda20e82f04e021bac1e77f65
DailyLollipops <clarencemadrigal84@gmail.com>
2022-09-10 16:38:51 +0800

    Made footer a component

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -36,11 +36,7 @@
     
     @yield('content')   
     
-    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-7727">
-      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
-          <p class="u-small-text u-text u-text-variant u-text-1">Made for Thesis: Med-bot: Pulse Rate and Blood Pressure Monitor<br>@2022
-          </p>
-      </div>
-    </footer>
+    <x-footer/>
+    
   </body>
 </html>
