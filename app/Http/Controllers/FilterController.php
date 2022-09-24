5b5fa7ab4e782792bfcda4532ba291b1f4620bc9
DailyLollipops <clarencemadrigal84@gmail.com>
2022-09-25 00:34:45 +0800

    Code optimization

--- app/Http/Controllers/FilterController.php
+++ app/Http/Controllers/FilterController.php
@@ -10,34 +10,50 @@ use App\Models\Reading;
 class FilterController extends Controller
 {
     //Get order of readings
-    public function getSelector(Request $request) {
+    public function getReadingSelector(Request $request) {
         $filter = explode('-',$request['filter']);
 
-        return redirect('/list/order-by-'.$filter[0].'-'.$filter[1]);
+        return redirect('/readinglist/order-by-'.$filter[0].'-'.$filter[1]);
     }
 
     //Order the readings
-    public function order($filter, $order) {
-        $readings = Reading::where('user_id',Auth::id())->paginate(9);
-        if($order == 'desc'){
-            $readings->setCollection(
-                collect(
-                    collect($readings->items())->sortByDesc($filter)
-                )->values()
-            );
+    public function orderReading($filter, $order) {
+        $readings = Reading::where('user_id',Auth::id())->orderBy($filter,$order)->paginate(9);
+        if($filter == 'created_at'){
+            $filterString = 'Date';
         }
         else{
-            $readings->setCollection(
-                collect(
-                    collect($readings->items())->sortBy($filter)
-                )->values()
-            );
+            $filterString = ucfirst(str_replace('_', ' ', $filter));
         }
-        $filterString = ucfirst(str_replace("_", " ", $filter));
-        $orderString = ucfirst($order."ending");
-        flash()->addInfo('Sorted by '.$filter.' '.$order);
+        $orderString = ucfirst($order.'ending');
+        flash()->addInfo('Sorted by '.$filterString.' ('.$orderString.')');
         return view('user.readinglist',[
             'readings' => $readings,
+            'filter' => $filterString,
+            'order' => $orderString,
+        ]);
+    }
+
+    //Get order of users
+    public function getUserSelector(Request $request) {
+        $filter = explode('-',$request['filter']);
+
+        return redirect('/userlist/order-by-'.$filter[0].'-'.$filter[1]);
+    }
+
+    // Order the users
+    public function orderUser($filter, $order) {
+        $users = User::where('type','normal')->orderBy($filter,$order)->paginate(10);
+        if($filter == 'birthday'){
+            $filterString = 'Age';
+        }
+        else{
+            $filterString = ucfirst(str_replace('_', ' ', $filter));
+        }
+        $orderString = ucfirst($order.'ending');
+        flash()->addInfo('Sorted by '.$filterString.' ('.$orderString.')');
+        return view('doctor.userlist',[
+            'users' => $users,
             'filter' => $filterString,
             'order' => $orderString,
         ]);
