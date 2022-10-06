7486a704356b13ca5052138897e588836b2fdcc9
DailyLollipops <clarencemadrigal84@gmail.com>
2022-10-06 18:02:54 +0800

    Changed date format

--- resources/views/user/readinglist.blade.php
+++ resources/views/user/readinglist.blade.php
@@ -37,7 +37,7 @@
                 <span class="u-file-icon u-icon">
                   <img src="{{ asset('images/calendar.png') }}" alt="">
                 </span>
-                <span style="font-weight: 700;">&nbsp;{{date('Y-m-d', strtotime($reading->created_at))}}
+                <span style="font-weight: 700;">&nbsp;{{date('M d, Y', strtotime($reading->created_at))}}
                   <span style="font-size: 1.125rem;"></span>
                 </span>
               </p>
