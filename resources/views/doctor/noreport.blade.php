5227d56d07781b4a4d26b846585605252951718e
DailyLollipops <clarencemadrigal84@gmail.com>
2022-12-31 12:43:23 +0800

    Changed displayed message if user has no readings for doctor view

--- resources/views/doctor/noreport.blade.php
+++ resources/views/doctor/noreport.blade.php
@@ -45,11 +45,11 @@
 <section class="u-clearfix u-section-2" id="sec-5d60">
   <div class="u-clearfix u-sheet u-sheet-1">
     <img class="u-image u-image-contain u-image-default u-image-1" src="{{ asset('images/nothing_found.png') }}" alt="" data-image-width="411" data-image-height="250">
-    <h4 class="u-text u-text-1">It seems you haven't taken any readings yet that's why we can't display anything here</h4>
+    <h4 class="u-text u-text-1">It seems that the user haven't taken any readings yet that's why we can't display anything here</h4>
     <h4 class="u-text u-text-default-lg u-text-default-md u-text-default-sm u-text-default-xl u-text-2">Suggestions:</h4>
     <ul class="u-text u-text-3">
       <li>
-        <span style="font-size: 24px;">Visit your local Med-bot and start measuring your vital signs</span>
+        <span style="font-size: 24px;">Have them visit local Med-bot and start measuring your vital signs</span>
       </li>
     </ul>
   </div>
